e7e4d6dc42b9e604d78bdc9d4ec43dacd3a8e702
Daniela Ramos <daniramos5566@hotmail.com>
2024-12-18 14:35:43 -0600

    Initial Commit

---
+++ colorWorld/colorWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorWorldApp.swift
+//  colorWorld
+//
+//  Created by Daniela Ramos Garcia on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct colorWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ colorWorldTests/colorWorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  colorWorldTests.swift
+//  colorWorldTests
+//
+//  Created by Daniela Ramos Garcia on 18/12/24.
+//
+
+import Testing
+@testable import colorWorld
+
+struct colorWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
